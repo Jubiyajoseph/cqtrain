628b911de4a29a804f6dae33aa2f3b7fab6d1b15
Jubiyajoseph <jubiyaaj15@gmail.com>
2023-07-10 17:20:28 +0530

    added c# asignt

---
+++ C#assignments/EmployeeManagement/EmployeeManagement/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement
+{
+    internal class Employee
+    {
+        public int Empid { get; set; }
+        public string Empname { get; set; }
+        public double Empsalary { get; set; }
+        public Employee(int id)
+        {
+            Empid = id;
+        }
+        public Employee(int id,string name) : this(id)
+        {
+            Empname = name;
+        }
+        public Employee(int id, string name, double salary) : this(id,name)
+        {
+            Empsalary = salary;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Employee Id=" + Empid);
+            Console.WriteLine("Name of the Employee=" + Empname);
+            Console.WriteLine("Salary of the Employee=" + Empsalary);
+        }
+    }
+}
---
+++ C#assignments/EmployeeManagement/EmployeeManagement/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EmployeeManagement;
+
+Employee e = new Employee(10,"Ju",25000.90);
+e.Display();
+
+
