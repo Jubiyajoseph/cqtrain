3255244188093180df04ef0d9350c994beff66ee
Jubiyajoseph <jubiyaaj15@gmail.com>
2023-07-11 18:41:01 +0530

    11th july assignment

---
+++ C#assignments/ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp7;
+
+Student student1 = new Student(1, "John", 85);
+student1.Display();
+Student student2 = new Student(2, "David", 74);
+student2.Display();
+Student student3 = new Student(3, "Thomas", 90);
+student3.Display();
---
+++ C#assignments/ConsoleApp7/ConsoleApp7/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Mark { get; set; }
+        public char Grade  { get;  }
+
+        public Student(int id,string name,int mark)
+        {
+            Id = id;
+            Name = name;
+            Mark = mark;
+            Grade = Calculate(Mark);
+        }
+        public char Calculate(int Mark)
+        {
+            if (Mark >= 90)
+                return 'A';
+            else if (Mark >= 80 && Mark < 90)
+                return 'B';
+            else if (Mark >= 70 && Mark < 80)
+                return 'C';
+            else if (Mark < 70)
+                return 'D';
+            else
+                return 'F';
+        }
+        public void Display()
+        {
+            Console.WriteLine("StudentId="+ Id);
+            Console.WriteLine("Srtudent name="+ Name);
+            Console.WriteLine("Mark="+ Mark);
+            Console.WriteLine("Grade : " + Grade);
+        }
+    }
+}
