e0401759a0be09750e38a2c504721441b54b03bc
Jubiyajoseph <jubiyaaj15@gmail.com>
2023-07-17 17:27:00 +0530

    added assignment1

---
+++ C#assignments/July 17th/ConsoleApp9/ConsoleApp9/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfJoin { get; set; }
+        public string Department { get; set; }
+        public double Salary { get; set; }
+        public char Jobtype  { get; set; }
+        public Employee(int id,string name,DateTime doj,string dept,double sal, char jobtype)
+        {
+            Id = id;
+            Name = name;
+            DateOfJoin = doj;
+            Department = dept;
+            Salary = sal;
+            Jobtype =jobtype;
+        }
+}
+    interface IBonusCalculator
+    {
+        double CalculateBonus(Employee emp);
+    }
+    class PermanentEmployeeBonusCalculator : IBonusCalculator
+    {
+        public double CalculateBonus(Employee emp)
+        {
+            return 0.2 * emp.Salary;
+        }
+    }
+    class ContractEmployeeBonusCalculator : IBonusCalculator
+    {
+        public double CalculateBonus(Employee emp)
+        {
+            return 0.15 * emp.Salary;
+        }
+    }
+}
---
+++ C#assignments/July 17th/ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp9;
+
+DateTime dt = new DateTime(2023, 12, 01);
+Employee emp = new Employee(1,"Jubiya",dt,"HR",29000.09, 'P');
+IBonusCalculator ibc = null;
+if (emp.Jobtype == 'P')
+{
+    ibc = new PermanentEmployeeBonusCalculator();
+}     
+else
+{
+    ibc = new ContractEmployeeBonusCalculator();
+}
+    
+double bonusAmt = ibc.CalculateBonus(emp);
+Console.WriteLine("Bonus amount:" + bonusAmt);
