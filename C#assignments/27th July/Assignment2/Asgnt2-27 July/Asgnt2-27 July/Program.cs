e8dfd4d1dfa50478e4233b98ed70fd0769309de0
Jubiyajoseph <jubiyaaj15@gmail.com>
2023-07-27 17:30:07 +0530

    added asgnt2

---
+++ C#assignments/27th July/Assignment2/Asgnt2-27 July/Asgnt2-27 July/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Asgnt2_27_July;
+
+Console.Write("Enter a character: ");
+try
+{
+    char input = Console.ReadKey().KeyChar;
+    Console.WriteLine("\nSports name=");
+    switch (input)
+    {
+        case 'c':
+            {
+                Console.WriteLine("Cricket");
+                break;
+            }
+        case 'f':
+            {
+                Console.WriteLine("Football");
+                break;
+            }
+        case 'h':
+            {
+                Console.WriteLine("Hockey");
+                break;
+            }
+        case 't':
+            {
+                Console.WriteLine("Tennis");
+                break;
+            }
+        case 'b':
+            {
+                Console.WriteLine("Badminton");
+                break;
+            }
+        default: throw new InvalidUserInputException("Invalid Input.");
+    }
+}
+catch (InvalidUserInputException e)
+{
+    Console.WriteLine(e.Message);
+}
+Console.ReadLine();
+
