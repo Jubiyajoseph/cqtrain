9a6be6e52adbaadc1f857cbd6ce76798ddeebada
Jubiyajoseph <jubiyaaj15@gmail.com>
2023-07-27 15:30:27 +0530

    added c# assignment1

---
+++ C#assignments/27th July/Assignment1/Employee.Model/Employee.Model/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeModel
+{
+    public class Employee
+    {
+        public string Id { get; private set; }
+        public string  Name { get; set; }
+        public double Salary { get; set; }
+        public  string  EmployeeType { get; set; }
+
+        private static int count;
+        static Employee()
+        {
+            count = 0;
+        }
+        public Employee(string name, double salary, string employeeType)
+        {
+            int a = count + 1000;
+            Id = "Emp" + a.ToString();
+            count++;
+            Name = name;
+            Salary = salary;
+            EmployeeType = employeeType;
+        }
+        public static int EmployeeCount()
+        {
+            return count;
+        }
+    }
+}
---
+++ C#assignments/27th July/Assignment1/Employee/Employee/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EmployeeModel;
+
+Employee emp1 = new Employee("John Doe",15000,"Contract");
+Employee emp2 = new Employee("Liam Smith", 20000,"Permanent");
+Employee emp3 = new Employee("Mary James", 15000,"Contract");
+
+int numberofEmployees = Employee.EmployeeCount();
+Console.WriteLine( "Total number of Employees="+ numberofEmployees);
