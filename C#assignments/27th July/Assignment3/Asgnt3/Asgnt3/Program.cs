ae179e35da21e3a56c105fbfa430e30dd7397149
Jubiyajoseph <jubiyaaj15@gmail.com>
2023-07-27 18:40:01 +0530

    added c# assignmentt3

---
+++ C#assignments/27th July/Assignment3/Asgnt3/Asgnt3/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Asgnt3;
+
+Console.WriteLine("Enter some words=");
+string input = Console.ReadLine();
+Console.WriteLine("Enter the word to find the occurence=");
+string word = Console.ReadLine();
+int value = StringExtensionClass.WordCount(input,word);
+Console.WriteLine("Occurence of the word "+ word + " is "+ value);
---
+++ C#assignments/27th July/Assignment3/Asgnt3/Asgnt3/StringExtensionClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asgnt3
+{
+    public static class StringExtensionClass
+    {
+        public static int WordCount(this string input, string word)
+        {
+            int count = 0;
+            string[] txt = input.Split(' ');
+
+            for (int i = 0; i < txt.Length; i++)
+            {
+                if (txt[i].Equals(word))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
