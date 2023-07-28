b930c0ca634ac4588bbe13a4cdf655ff6a195c8b
Jubiyajoseph <jubiyaaj15@gmail.com>
2023-07-28 11:25:11 +0530

    added assignment4

---
+++ C#assignments/27th July/Assignment4/Qn4/Qn4/MyGenericComparer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qn4
+{
+    public class MyGenericComparer<T> where T : IComparable            // Implementing IComparable interface
+    {
+        public T Compare(T val1, T val2)
+        {
+            if(val1.CompareTo(val2)>0)
+            {
+                return val1;
+            }
+            else
+            {
+                return val2;
+            }
+        }
+    }
+}
---
+++ C#assignments/27th July/Assignment4/Qn4/Qn4/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Qn4;
+MyGenericComparer<int> obj1 = new MyGenericComparer<int>();
+Console.WriteLine("Larger value= "+ obj1.Compare(5,8));
+
+MyGenericComparer<double>obj2 = new MyGenericComparer<double>();
+double value = obj2.Compare(16.9, 5.25);
+Console.WriteLine("Larger Value= " + value);
+
+MyGenericComparer<string> obj3 = new MyGenericComparer<string>();
+string val = obj3.Compare("Hi", "hi");
+Console.WriteLine("Larger Value= " + val);
+
