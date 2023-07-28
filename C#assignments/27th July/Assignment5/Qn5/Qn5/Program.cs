31dfde41147f7995de8198d0e50496995b4cd4ce
Jubiyajoseph <jubiyaaj15@gmail.com>
2023-07-28 09:31:11 +0530

    added assignment5

---
+++ C#assignments/27th July/Assignment5/Qn5/Qn5/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] Array = { 1, 2, 3, 4, 5 };
+
+List<int> newList = new List<int>();
+
+Console.WriteLine("Array elements\n");
+foreach (int num in Array)
+{
+    Console.WriteLine(num);
+    newList.Add(num * 2);
+}
+Console.WriteLine("\nNew Elements  ");
+foreach (int i in newList)
+{
+    Console.WriteLine(i );
+}
