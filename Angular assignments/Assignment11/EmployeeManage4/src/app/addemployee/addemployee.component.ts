ba3646202694634a04f8667fc164b905ce8578e6
Jubiyajoseph <jubiyaaj15@gmail.com>
2023-08-21 18:47:59 +0530

    updated

--- Angular assignments/Assignment11/EmployeeManage4/src/app/addemployee/addemployee.component.ts
+++ Angular assignments/Assignment11/EmployeeManage4/src/app/addemployee/addemployee.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { IEmployeeData } from '../employee/IEmployeeData';
 import { EmployeesService } from '../employee/employees.service';
 
@@ -8,7 +8,7 @@ import { EmployeesService } from '../employee/employees.service';
   templateUrl: './addemployee.component.html',
   styleUrls: ['./addemployee.component.sass']
 })
-export class AddemployeeComponent {
+export class AddemployeeComponent implements OnInit{
 
   //public emp: Array<IEmployeeData>=[];
   public newEmp: IEmployeeData={
@@ -30,19 +30,35 @@ export class AddemployeeComponent {
     country: new FormControl(''),
   })
 
-  constructor(private empService: EmployeesService)
+  constructor(private empService: EmployeesService,private fb: FormBuilder)
   {
+  }
+
+  ngOnInit(): void
+  {
+    this.empForm = this.fb.group({
+      firstName: new FormControl('',[Validators.minLength(5),Validators.required]),
+      lastName: new FormControl('',[Validators.maxLength(15),Validators.required]),
+      dateOfBirth: new FormControl('',[Validators.required]),
+      age: new FormControl(''),
+      dateOfJoining: new FormControl('',[Validators.required]),
+      country: new FormControl('',[Validators.required])
+    });
   }
 
   onClickAddEmp()
   {
-    this.newEmp.id=this.empService.empList[this.empService.empList.length-1].id + 1
-    this.newEmp.firstName = this.empForm.get('firstName')?.value!;
-    this.newEmp.lastName = this.empForm.get('lastName')?.value!;
-    this.newEmp.dateOfBirth = new Date(this.empForm.get('dateOfBirth')?.value!);
-    this.newEmp.age= Number(this.empForm.get('age')?.value);
-    this.newEmp.dateOfJoining = new Date(this.empForm.get('dateOfJoining')?.value!);
-    this.newEmp.country = this.empForm.get('country')?.value!;
-    this.empService.empList.push(this.newEmp);
+    if(this.empForm.valid)
+    {
+      this.newEmp.id=this.empService.empList[this.empService.empList.length-1].id + 1
+      this.newEmp.firstName = this.empForm.get('firstName')?.value!;
+      this.newEmp.lastName = this.empForm.get('lastName')?.value!;
+      this.newEmp.dateOfBirth = new Date(this.empForm.get('dateOfBirth')?.value!);
+      this.newEmp.age= Number(this.empForm.get('age')?.value);
+      this.newEmp.dateOfJoining = new Date(this.empForm.get('dateOfJoining')?.value!);
+      this.newEmp.country = this.empForm.get('country')?.value!;
+      this.empService.empList.push(this.newEmp);
+    }
+
   }
 }
